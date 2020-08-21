4bbd8c4e8968f3d99b0cb9fb801fbd0c60d1d2a0
vinayak <vinayak.sohal007@gmail.com>
2020-08-21 17:44:58 -0400

    Question 2

---
+++ VogCodeChallenge.API/Models/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VogCodeChallenge.API.Models
+{
+    public class Employee
+    {
+            public int Id { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string JobTitle { get; set; }
+            public string AddressOfResidence { get; set; }
+            [ForeignKey("DepartmentId")]
+            [NotMapped]
+            public Department Departments;
+            public int DepartmentId { get; set; }
+    }
+}
