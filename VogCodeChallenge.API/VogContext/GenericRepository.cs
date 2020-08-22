88e14382e0a1614094b7166fa97905227352b85e
vinayak <vinayak.sohal007@gmail.com>
2020-08-21 22:00:40 -0400

    Question 7

---
+++ VogCodeChallenge.API/VogContext/GenericRepository.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace VogCodeChallenge.API.VogContext
+{
+    public class GenericRepository<T> : IDataRepository<T> where T : class
+    {
+        public IEnumerable<T> GetAll(params Expression<Func<T, object>>[] navigationProperties)
+        {
+            IEnumerable<T> list;
+            using (var context = new VogCodeContext())
+            {
+                IQueryable<T> dbQuery = context.Set<T>();
+
+                foreach (Expression<Func<T, object>> navigationProperty in navigationProperties)
+                    dbQuery = dbQuery.Include<T, object>(navigationProperty);
+
+                list = dbQuery
+                    .AsNoTracking()
+                    .ToList<T>();
+            }
+            return list;
+        }
+
+        public IList<T> ListAll(Expression<Func<T, bool>> where, params Expression<Func<T, object>>[] navigationProperties)
+        {
+            List<T> list;
+            using (var context = new VogCodeContext())
+            {
+                IQueryable<T> dbQuery = context.Set<T>();
+
+                foreach (Expression<Func<T, object>> navigationProperty in navigationProperties)
+                    dbQuery = dbQuery.Include<T, object>(navigationProperty);
+
+                list = dbQuery
+                    .AsNoTracking()
+                    .Where(where)
+                    .ToList<T>();
+            }
+            return list;
+        }
+    }
+}
---
+++ VogCodeChallenge.API/VogContext/VogCodeContext.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using VogCodeChallenge.API.Models;
+
+namespace VogCodeChallenge.API.VogContext
+{
+    public class VogCodeContext : DbContext
+    {
+        protected readonly string _connStr = string.Empty;
+        public virtual DbSet<Employee> Employees { get; set; }
+        public virtual DbSet<Department> Departments { get; set; }
+
+        public VogCodeContext()
+        {
+            var config = new ConfigurationBuilder();
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "appsettings.json");
+            config.AddJsonFile(path, false);
+            var root = config.Build();
+            _connStr = root.GetSection("ConnectionStrings").GetSection("DataConnection").Value;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_connStr);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<Employee>()
+                        .HasOne<Department>(s => s.Departments)
+                        .WithMany(g => g.Employees)
+                        .HasForeignKey(s => s.DepartmentId);
+        }
+    }
+}
