68037f67883936f6902a591b19b1b4c604fc1286
vinayak <vinayak.sohal007@gmail.com>
2020-08-21 22:03:54 -0400

    Question 8

---
+++ ClassLibrary1/QuestionClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QuestionClass
+{
+    public static class QuestionClass
+    {
+        static List<string> NamesList = new List<string>()
+        {
+            "Jimmy",
+            "jeffrey"
+        };
+
+        public static void TestQuestion()
+        {
+            
+            using (var enumerator = NamesList.GetEnumerator())
+            {
+                while (enumerator.MoveNext())
+                {
+                    Console.WriteLine(enumerator.Current);
+                }
+            }
+
+            //alternate Implementation of above code
+            foreach (string s in NamesList)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
