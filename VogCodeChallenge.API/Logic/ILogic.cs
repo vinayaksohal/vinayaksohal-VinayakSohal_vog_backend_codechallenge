52e1b197f306dbd886b4482c1a82e461600b0443
vinayak <vinayak.sohal007@gmail.com>
2020-08-25 00:04:54 -0400

    Question 7 v7.2

---
+++ VogCodeChallenge.API/Logic/ILogic.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace VogCodeChallenge.API.Logic
+{
+    public interface ILogic<T>
+    {
+        public List<T> GetEmployeeList();
+        
+    }
+}
